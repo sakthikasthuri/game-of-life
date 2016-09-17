383daf3ae62e241d3fb6f60933151557a24f47b1
Ninja-kitten <tran260@mail.chapman.edu>
2016-09-16 23:36:48 -0700

    Update Cell.h

--- Cell.h
+++ Cell.h
@@ -1,10 +1,20 @@
 public:
 void setPosition();
-void setContent();
+//sets the position of the cell
+void fillCell();
+//fills a cell
+void emptyCell();
+//empties a cell
 int[] getPosition();
+//returns the row and column array
 int getRow;
+//returns just the row
 int getColumn;
+//returns just the column
 char getContent;
+//returns what's inside the cell either X or -
 private:
 int position[2]
+//a 1X2 array that shows the position RXC
 char content;
+//either a X or a -
