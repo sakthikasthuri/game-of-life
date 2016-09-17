7e41297868148c70b71da2af5d965a3997f189d2
Ninja-kitten <tran260@mail.chapman.edu>
2016-09-16 23:47:57 -0700

    Update GameofLife.h

--- GameofLife.h
+++ GameofLife.h
@@ -1,13 +1,22 @@
 #include "GameBoard.h"
 
+class GameofLife{
 public:
 GameofLife();
+//Constructor
 ~GameofLife();
-nextGen();
-
+//destructor
+void nextGen();
+//Moves the gameboard over to the next generation
 
 private:
+boundarymode boundary();
+//chooses the boundary taurus 
+pausemode pause();
+//s
 GameBoard oldBoard();
+//Gameboard before the generation change
 GameBoard newBoard();
-
+//GameBoard after the generation shifts
+}
 
