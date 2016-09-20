3f95ce992e7ebafc1a0df24a918089edf0f718bb
sakthikasthuri <kasth100@mail.chapman.edu>
2016-09-19 20:16:39 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "GameBoard.h"
+#include "GameOfLife.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+cout << "Welcome to the Game of Life!" << endl;
+cout << "Would you like to start by providing a file to run the simulation or would you like a random assignment?"<< endl;
+cout << "Please enter 'file' or 'random' to start the setup." << endl;
+string answer;
+cin >> answer;
+if(answer == "file")
+{
+  //read file and exptrapolate info
+}
+else if(answer == "random")
+{
+  cout << "Please enter the number of rows you'd like in your grid: "<<endl;
+  cin >> int i;
+  cout << "Please enter the number of columns you'd like in your grid: "<<endl;
+  cin >> int j;
+}
