bfa59d05fd7061b85670fada9d2b2eb2eae8d6c9
sakthikasthuri <kasth100@mail.chapman.edu>
2016-09-20 22:19:18 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 public main(int argc, char** argv){
 
 GameOfLife gL;
+
 cout << "Welcome to the Game of Life!" << endl;
 cout << "Would you like to start by providing a file to run the simulation or would you like a random assignment?"<< endl;
 cout << "Please enter 'file' or 'random' to start the setup." << endl;
@@ -14,7 +15,21 @@ string answer;
 cin >> answer;
 if(answer == "file")
 {
-  //read file and exptrapolate info
+  string filename;
+  cout << "Please enter the name of the file: " << endl;
+  cin >> filename;
+  
+  cout << "To determine the type of boundary, please enter: 1 for Classic, 2 for Doughnut or 3 for Mirror."<< endl;
+  cin >> string strboundary;
+  int boundary = stoi(strboundary,nullptr);
+  
+  cout<< "To determine the type of display, please enter: 1 for Pause, 2 for Enter or 3 for File." << endl;
+  cin >> string strMode;
+  int mode = stoi(strMode,nullptr);
+  
+  if((boundary == 1 || boundary == 2 || boundary == 3) && (mode == 1 || mode == 2 || mode == 3)){
+    gL.GameOfLife(boundary,mode, filename);}
+  else{ throw invalid_argument("Please enter the specified integers");}
 }
 else if(answer == "random")
 {
