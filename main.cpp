9d2e9979c2d19307e04e480bd33d2c15dfcb6447
sakthikasthuri <kasth100@mail.chapman.edu>
2016-09-20 20:07:54 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 using namespace std;
 
 public main(int argc, char** argv){
+
+Gameboard gb;
 cout << "Welcome to the Game of Life!" << endl;
 cout << "Would you like to start by providing a file to run the simulation or would you like a random assignment?"<< endl;
 cout << "Please enter 'file' or 'random' to start the setup." << endl;
@@ -18,10 +20,18 @@ if(answer == "file")
 else if(answer == "random")
 {
   cout << "Please enter the number of rows you'd like in your grid: "<<endl;
-  cin >> int i;
+  cin >> int numRow;
   cout << "Please enter the number of columns you'd like in your grid: "<<endl;
-  cin >> int j;
+  cin >> int numCol;
+  gb.GameBoard(numRow,numCol);
   cout << "Please enter a number and number between 0 and 1 to represent the density of filled spaces in your grid: "<< endl;
-  cin >> float k;
+  cin >> float density;
+  
+  
+}
+else
+{
+ throw invalid_argument("Please enter file or random"); 
+}
 }
 }
